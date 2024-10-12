99b6b8faee33bccbfcc8264502d86a078200bded
Harsh <63919951+harsh11x@users.noreply.github.com>
2024-10-13 02:48:13 +0530

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-import yaml
-
-def load_config(config_file):
-    """Load configuration settings from a YAML file."""
-    with open(config_file, 'r') as file:
-        config = yaml.safe_load(file)
-    return config
